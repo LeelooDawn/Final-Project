7b0bb415e376355f48255a94d8c3cf106fdd9ef2
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2023-03-08 15:29:39 -0700

    Added index and updated app & index to connect

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request
-import sqlite3
+import sqlite3 as db
 
 
 app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template ("index.html", name=name)
+    else:
+        return render_template ("index.html", name="YOU")
