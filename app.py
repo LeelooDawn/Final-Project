9bfa658182b3141730f3f5db5fa68f08937821f8
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2023-03-09 12:31:13 -0700

    added style and changed index to register

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
+from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3 as db
 
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/register", methods=["GET", "POST"])
+def register():
     if request.method == "POST":
-        name = request.form.get("name")
-        return render_template ("index.html", name=name)
-    else:
-        return render_template ("index.html", name="YOU")
+        name = request.form.get("username")
+        password = request.form.get("password")
+        email = request.form.get("email")
+        
