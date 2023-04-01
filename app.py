7d646db42c439118aa3dbe0d4b52f5e94f2952ce
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2023-04-01 12:46:22 -0600

    Log In Created Successfully

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import sqlite3
 
 app = Flask(__name__)
 
+#configure SQL database
+db = "/users/leslienesbit/Documents/GitHub Projects/Final Project/potluck.db"
 #make sure API key is set
 
 #configure session to use filesystem instead of signed cookies
@@ -14,7 +16,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-#app route index (profile page) @login-required
+#app route index (profile page)
 @app.route("/")
 def index():
 #show profile of user
@@ -32,17 +34,17 @@ def login():
         password = request.form.get("password")
 
         if not username:
-            return ("Must Provide Username")
+            return render_template("error.html", error="no username")
         elif not password:
-            return ("Must Provide Password") 
+            return render_template("error.html", error="no password") 
         #query database for username
-        with sqlite3.connect("/users/leslienesbit/Documents/GitHub Projects/Final Project/potluck.db") as con:
-            con.row_factory = sqlite3.Row
+        with sqlite3.connect(db) as con:
             cur = con.cursor()
-            rows = con.execute("SELECT * FROM users WHERE username = ?", (username,))
-            row = rows.fetchone()[0]
+            query = ("SELECT * FROM users WHERE username = ?", (username,))
+            cur.execute(*query)
+            user = cur.fetchone()
         
-        return render_template("/")
+        return render_template("index.html", username=user[1])
     else:
         return render_template("login.html")
 
@@ -59,36 +61,28 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        try:
-            username = request.form.get("username")
-            password = request.form.get("password")
-            email = request.form.get("email")
-            if not username:
-                return "Please enter username"
-            elif not password:
-                return "Please enter password"
-            elif not email:
-                return "Please enter email"
+        username = request.form.get("username"),
+        password = request.form.get("password"),
+        email = request.form.get("email") 
+   
         #generate hash of password
-            hash = generate_password_hash(password)
-    
-        #enter new user into database
-            with sqlite3.connect("/users/leslienesbit/Documents/GitHub Projects/Final Project/potluck.db") as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO users (username, hash, email) VALUES (?,?,?)", (username, hash, email))
-        #activate new session for user
-                con.commit()
-                message = "User successfully added"
-                #activate new session for user
-                #new_user= cur.execute("SELECT id FROM users WHERE username = :username", username=username)
-                #session["user_id"] = new_user[0]["id"]
-        except:
-            con.rollback()
-        #redirect to index page
-        finally:
-            return redirect("/login")
-            con.close()
-        #else via GET redirect to register page
+        hash = generate_password_hash(password)
+        add_user(username, hash, email)
+        return render_template("index.html", username=username)
     else:
         return render_template("register.html")
+
+        #enter new user into database
+def add_user(username, hash, email):
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    insert_user = ("INSERT INTO users (username, hash, email) VALUES (?,?,?)", (username, hash, email))
+    cur.execute(insert_user)
+    con.commit()
+    con.close()
+                #activate new session for user
+                #new_user= cur.execute("SELECT id FROM users WHERE username = :username", username=username)
+                #session["user_id"] = new_user[0]["id]
+if __name__ == '__main__':
+    app.run()  
 
