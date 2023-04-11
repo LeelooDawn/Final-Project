6ec4a58af57fda3b737098e9e689a7aca090c046
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2023-04-11 11:19:46 -0600

    creation of recipe html

--- app.py
+++ app.py
@@ -127,6 +127,10 @@ def event():
    
     return render_template("event.html")
 
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
 if __name__ == '__main__':
     app.run()  
 
